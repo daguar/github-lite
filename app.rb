602a00dbf6acc9b5e33986e48daa703ab4079f8f
Andrew Hyder <hyd415@gmail.com>
2014-11-12 14:04:05 -0800

    Replaced octokit with the authenticated user.

--- app.rb
+++ app.rb
@@ -19,10 +19,10 @@ get '/' do
     A list of all the discussions happening in the main repo.
   '''
   @main_repo = "daguar/github-lite"
-  @issues = Octokit.list_issues(@main_repo)
+  @issues = github_user.api.list_issues(@main_repo)
   # Show each issues labels as well.
   for issue in @issues do
-    issue.labels = Octokit.labels_for_issue("#{@main_repo}", "#{issue.number}")
+    issue.labels = github_user.api.labels_for_issue("#{@main_repo}", "#{issue.number}")
   end
   erb :index
 end
@@ -39,13 +39,13 @@ end
 
 get '/:username/:repo_name/?' do
   @repo_string = "#{params[:username]}/#{params[:repo_name]}"
-  @readme_html = Octokit.readme "#{params[:username]}/#{params[:repo_name]}", :accept => 'application/vnd.github.html'
+  @readme_html = github_user.api.readme "#{params[:username]}/#{params[:repo_name]}", :accept => 'application/vnd.github.html'
   erb :repo_index
 end
 
 get '/:username/:repo_name/discussion/?' do
   @repo_string = "#{params[:username]}/#{params[:repo_name]}"
-  @issues = Octokit.list_issues("#{params[:username]}/#{params[:repo_name]}")
+  @issues = github_user.api.list_issues("#{params[:username]}/#{params[:repo_name]}")
   @url = "/#{params[:username]}/#{params[:repo_name]}/discussion"
   erb :issues_layout do
     erb :issues
@@ -55,8 +55,8 @@ end
 get '/:username/:repo_name/discussion/:issue_number/?' do
   @repo_string = "#{params[:username]}/#{params[:repo_name]}"
   issue_number = params[:issue_number]
-  @issue = Octokit.issue(@repo_string, issue_number, :accept => 'application/vnd.github.html')
-  @comments = Octokit.issue_comments(@repo_string, issue_number, :accept => 'application/vnd.github.html')
+  @issue = github_user.api.issue(@repo_string, issue_number, :accept => 'application/vnd.github.html')
+  @comments = github_user.api.issue_comments(@repo_string, issue_number, :accept => 'application/vnd.github.html')
   erb :issues_layout do
     erb :issue
   end
@@ -64,6 +64,6 @@ end
 
 post '/:username/:repo_name/discussion/:issue_number' do
   authenticate!
-  Octokit.add_comment("#{params[:username]}/#{params[:repo_name]}", "#{params[:issue_number]}", "#{params[:comment]}")
+  github_user.api.add_comment("#{params[:username]}/#{params[:repo_name]}", "#{params[:issue_number]}", "#{params[:comment]}")
   redirect "/#{params[:username]}/#{params[:repo_name]}/discussion/#{params[:issue_number]}"
 end
