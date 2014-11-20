53c59ce5c1034d859b0775638dd067ad8bc473c5
Andrew Hyder <hyd415@gmail.com>
2014-11-20 11:41:07 -0800

    Prototype ready

--- app.rb
+++ app.rb
@@ -23,13 +23,14 @@ get '/forum/logout' do
 end
 
 # The main repository for our discussions
-main_repo = "daguar/github-lite"
+main_repo = "codeforamerica/health"
 
 get '/forum' do
   '''
     A list of all the discussions happening in the main repo.
   '''
   if authenticated?
+    @labels = github_user.api.labels(main_repo)
     @issues = github_user.api.list_issues(main_repo)
     # Show each issues labels as well.
     for issue in @issues do
@@ -37,6 +38,7 @@ get '/forum' do
     end
   end
   if not authenticated?
+    @labels = Octokit.labels(main_repo)
     @issues = Octokit.list_issues(main_repo)
     # Show each issues labels as well.
     for issue in @issues do
@@ -47,8 +49,27 @@ get '/forum' do
 end
 
 get '/forum/i/:issue_number/?' do
-  issue_number = params[:issue_number]
-  @issue = github_user.api.issue("#{main_repo}", issue_number, :accept => 'application/vnd.github.html')
-  @comments = github_user.api.issue_comments("#{main_repo}", issue_number, :accept => 'application/vnd.github.html')
+  if authenticated?
+    @issue = github_user.api.issue("#{main_repo}", "#{params[:issue_number]}")
+    @comments = github_user.api.issue_comments("#{main_repo}", "#{params[:issue_number]}")
+  end
+  if not authenticated?
+    @issue = Octokit.issue("#{main_repo}", "#{params[:issue_number]}")
+    @comments = Octokit.issue_comments("#{main_repo}", "#{params[:issue_number]}")
+  end
+  erb :issue
+end
+
+post '/forum/i/:issue_number' do
+  authenticate!
+  github_user.api.add_comment("#{main_repo}", "#{params[:issue_number]}", "#{params[:comment]}")
+  redirect "/forum/i/#{params[:issue_number]}"
+end
+
+get '/forum/i/:issue_number/edit' do
+  if authenticated?
+    @issue = github_user.api.issue("#{main_repo}", "#{params[:issue_number]}")
+    @comments = github_user.api.issue_comments("#{main_repo}", "#{params[:issue_number]}")
+  end
   erb :issue
 end
